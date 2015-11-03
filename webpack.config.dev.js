a2623eaa81860da693f276675f2bf32ed5741336
Peter Moresi <peter.moresi@gmail.com>
2015-11-02 19:18:59 -0800

    fixes

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -12,12 +12,13 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/static/'
   },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
   module: {
     loaders: [{
-      test: /\.jsx?$/,
+      test: /\.js$/,
       loaders: ['babel'],
       include: path.join(__dirname, 'src')
     }]
