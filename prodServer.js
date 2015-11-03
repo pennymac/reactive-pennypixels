c0e0caeddabac8037d74dd99ac5f38356604c5e0
Peter Moresi <peter.moresi@gmail.com>
2015-11-02 21:29:56 -0800

    Update server

---
+++ prodServer.js
@@ -0,0 +1,18 @@
+var path = require('path');
+var express = require('express');
+var config = require('./webpack.config.prod');
+
+var app = express();
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(3000, 'localhost', function(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log('Listening at http://localhost:3000');
+});
