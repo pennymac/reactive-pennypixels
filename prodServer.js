162209cd52cb48d9c9abac28029f6761b0aaca44
Peter Moresi <peter.moresi@gmail.com>
2015-11-02 22:01:16 -0800

    update server

--- prodServer.js
+++ prodServer.js
@@ -1,20 +1,15 @@
-var path = require('path');
 var express = require('express');
-var config = require('./webpack.config.prod');
-
 var app = express();
+var path = require('path');
 
-app.use('/static', express.static('dist'));
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(5000, function(err) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-
-  console.log('Listening at http://*:5000');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
