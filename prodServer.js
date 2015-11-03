405669efa575434cf4256c0284c1192857dad888
Peter Moresi <peter.moresi@gmail.com>
2015-11-02 21:32:26 -0800

    Update server

--- prodServer.js
+++ prodServer.js
@@ -8,11 +8,11 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, 'localhost', function(err) {
+app.listen(80, '*', function(err) {
   if (err) {
     console.log(err);
     return;
   }
 
-  console.log('Listening at http://localhost:3000');
+  console.log('Listening at http://*:3000');
 });
