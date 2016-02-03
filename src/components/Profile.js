e43fc225df2a562f7445a2e9af44aaa7dd61bdf5
Peter Moresi <peter.moresi@pnmac.com>
2016-02-03 14:38:34 -0800

    Update profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -43,7 +43,7 @@ const ProfilePage = React.createClass({
         <div className="row">
           <div col="col-md-6">
             <b>Username: </b>
-            <span>{ this.state.user.username }</span>
+            <span>{ this.state.user.user_name }</span>
             &nbsp;&nbsp;
             <a href="/profile/username">Edit</a>&nbsp;&nbsp;|&nbsp;&nbsp;
             <a href="/profile/password">Edit Password</a>
@@ -67,13 +67,13 @@ const ProfilePage = React.createClass({
         <div className="row">
           <div col="col-md-6">
             <b>Phone: </b>
-            <span>{ this.state.user.mobile_phone }</span>
+            <span>{ this.state.user.phone_number }</span>
             &nbsp;&nbsp;
             <a href="/profile/phone">Edit</a>
           </div>
           <div col="col-md-6">
             <b>Has TCPA?: </b>
-            <span>{this.state.user.tcpa_flag ? 'Yes' : 'No' }</span>
+            <span>{this.state.user.phone_tcpa ? 'Yes' : 'No' }</span>
           </div>
         </div>
 
@@ -87,7 +87,7 @@ const ProfilePage = React.createClass({
 
         <div className="row">
           <div col="col-md-6">
-            <b>Security options: </b>
+            <b>Security options: </b>{this.state.user.current_mfa_type}
             &nbsp;&nbsp;
             <a href="/profile/security_options">Edit</a>
           </div>
